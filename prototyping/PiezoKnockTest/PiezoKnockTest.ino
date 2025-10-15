614a3d348667ce4ca34d4d814ee40a5396218338
Abhinav Kothari <abhinav.kothari@hsrw.org>
2025-10-15 19:22:41 +0200

    Added prototype testing code and video

---
+++ prototyping/PiezoKnockTest/PiezoKnockTest.ino
@@ -0,0 +1,32 @@
+/*
+  Knock Sensor (3.3V analog reference version)
+*/
+
+const int ledPin = 13;       // LED connected to digital pin 13
+const int knockSensor = A0;  // the piezo is connected to analog pin 0
+const int threshold = 50;    // lower threshold, since sensitivity increased
+
+int sensorReading = 0;
+int ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  // Use external reference (3.3V connected to AREF pin)
+  analogReference(EXTERNAL);
+  Serial.println("Knock sensor using 3.3V analog reference");
+}
+
+void loop() {
+  sensorReading = analogRead(knockSensor);
+
+  if (sensorReading >= threshold) {
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+    Serial.print("Knock!  Value: ");
+    Serial.println(sensorReading);
+  }
+
+  delay(50);
+}
